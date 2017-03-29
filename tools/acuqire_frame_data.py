017d9197929a2bc245f192af43034ff1d2b89a9e
CTTC <chentao904@163.com>
2017-03-29 10:16:12 +0800

    add tools

---
+++ tools/acuqire_frame_data.py
@@ -0,0 +1,292 @@
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from datetime import datetime
+from datetime import timedelta
+import Queue
+import os
+import time
+import json
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest, \
+    SetModelState, SetModelStateRequest, ApplyBodyWrench, ApplyBodyWrenchRequest
+from geometry_msgs.msg import Wrench, Pose
+import cv2
+import collections
+from cv_bridge import CvBridge, CvBridgeError
+import cv_bridge
+import threading
+from sensor_msgs.msg import Image
+import message_filters
+
+FrameData = collections.namedtuple('FrameData', 'left, right, ball_pos')
+PredictionData = collections.namedtuple('PredictionData', 'position, frame')
+
+class RepeatedTimer(object):
+  def __init__(self, interval, function, *args, **kwargs):
+    self._timer = None
+    self.interval = interval
+    self.function = function
+    self.args = args
+    self.kwargs = kwargs
+    self.is_running = False
+    self.next_call = time.time()
+    self.start()
+
+  def _run(self):
+    self.is_running = False
+    self.start()
+    self.function(*self.args, **self.kwargs)
+
+  def start(self):
+    if not self.is_running:
+      self.next_call += self.interval
+      self._timer = threading.Timer(self.next_call - time.time(), self._run)
+      self._timer.start()
+      self.is_running = True
+
+  def stop(self):
+    self._timer.cancel()
+    self.is_running = False
+
+
+class SaveFrame:
+    def __init__(self, camera):
+        self.camera = camera
+        self.image_frequency = 30.0
+        self.image_period = 1 / float(self.image_frequency)
+        self.image_ROSRate = rospy.Rate(self.image_frequency)
+        self.frames_data = Queue.Queue()
+        self.data_dir='./raw_data/'
+        if not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+        self.offline_action_filename = 'plan_point.json'
+
+    def subscribe_frames_data(self):
+        meta_data_one_frame = self.camera.get_camera_data()
+        if meta_data_one_frame is None:
+            self.frames_data = Queue.Queue()
+        else:
+            self.frames_data.put(meta_data_one_frame)
+        # self.end = time.time()
+        # print 'time elapsed: ', self.end - self.start, ' Hz:', 1 / float(self.end - self.start)
+        # self.start = self.end
+
+
+    def acquire_frames_data(self, num_images=30):
+        # self.start = time.time()
+        self.acquire_repeated_timer = RepeatedTimer(self.image_period, self.subscribe_frames_data)
+        while self.frames_data.qsize() < num_images:
+            pass
+        self.acquire_repeated_timer.stop()
+        meta_data = []
+        while not self.frames_data.empty():
+            meta_data_one_frame = self.frames_data.get()
+            meta_data.append(meta_data_one_frame)
+        return meta_data
+
+    def save_data(self, fx, fy, fz, meta_data, f_idx, h_idx):
+        ball_poses = []
+        f_folder = os.path.join(self.data_dir, 'f%d'%f_idx)
+        h_folder = os.path.join(f_folder, 'h%d'%h_idx)
+        left_folder = os.path.join(h_folder, 'left')
+        right_folder = os.path.join(h_folder, 'right')
+        xyz_folder = os.path.join(h_folder, 'xyz')
+        for folder in [left_folder, right_folder, xyz_folder]:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+        f_file = os.path.join(f_folder, 'force.txt')
+        f_data = {'fx': fx, 'fy': fy, 'fz': fz}
+        with open(f_file, 'w+') as f:
+            json.dump(f_data, f, indent=4)
+
+        for idx, meta_data_one_frame in enumerate(meta_data):
+            left_image = meta_data_one_frame.left
+            right_image = meta_data_one_frame.right
+            ball_pos = meta_data_one_frame.ball_pos
+            ball_poses.append(ball_pos)
+            left_image_filename = os.path.join(left_folder, 'left-%d.jpg'%idx)
+            right_image_filename = os.path.join(right_folder, 'right-%d.jpg' % idx)
+            xyz_filename = os.path.join(xyz_folder, 'xyz-%d.json'%idx)
+            cv2.imwrite(left_image_filename, left_image)
+            cv2.imwrite(right_image_filename, right_image)
+            xyz_data = {}
+            xyz_data['xyz'] = []
+            xyz_data['xyz'].append({
+                'x': ball_pos[0],
+                'y': ball_pos[1],
+                'z': ball_pos[2],
+            })
+            with open(xyz_filename, 'w+') as f:
+                json.dump(xyz_data, f, indent=4)
+        return ball_poses
+
+    def set_ball_position(self, px=0.7, py=-0.5):
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            set_position_re = SetModelStateRequest()
+            pose = Pose()
+            pose.position.x = px
+            pose.position.y = py
+            pose.position.z = 0.85
+            pose.orientation.x = 0
+            pose.orientation.y = 0
+            pose.orientation.z = np.sin(np.pi / 2)
+            pose.orientation.w = np.cos(np.pi / 2)
+            set_position_re.model_state.model_name = 'Ping Pong Ball'
+            set_position_re.model_state.pose = pose
+            set_state(set_position_re)
+            time.sleep(3)
+        except rospy.ServiceException, e:
+            raise "Service call failed: %s" % e
+
+
+    def hit_ball(self, fx=0.0, fy=0.15, fz=0.0, dt=0.1):
+        rospy.wait_for_service('/gazebo/apply_body_wrench')
+        try:
+            apply_force = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
+            apply_force_re = ApplyBodyWrenchRequest()
+            apply_force_re.body_name = 'Ping Pong Ball::ball'
+            wrench = Wrench()
+            wrench.force.x = fx
+            wrench.force.y = fy
+            wrench.force.z = fz
+            wrench.torque.x = 0
+            wrench.torque.y = 0
+            wrench.torque.z = 0
+            apply_force_re.wrench = wrench
+            apply_force_re.start_time = rospy.get_rostime()
+            apply_force_re.duration = rospy.Duration(dt)
+            apply_force(apply_force_re)
+        except rospy.ServiceException, e:
+            print("Service call failed: %s" % e)
+
+    def generate_force(self, bounds, num):
+        yz = np.mgrid[bounds[0][0]: bounds[0][1]: complex(num[0]),
+              bounds[1][0]: bounds[1][1]: complex(num[1])].reshape(2, -1).T
+        for i in range(yz.shape[1]):
+            yz[:, i] = yz[:, i] if num[i] > 1 else bounds.mean(axis=1)[i]
+        return yz
+
+
+class Camera:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self._br = cv_bridge.CvBridge()
+        self._cameraRight_img = None
+        self._cameraLeft_img = None
+        self._ball_pos = None
+        self._meta_data = None
+        self._observation_stale = True
+        self.camera_l_sub = message_filters.Subscriber("/multi/camera/basler/left/image_raw",
+                                                       Image)
+        self.camera_r_sub = message_filters.Subscriber("/multi/camera/basler/right/image_raw",
+                                                       Image)
+
+        self.sync = message_filters.ApproximateTimeSynchronizer([self.camera_l_sub, self.camera_r_sub],
+                                                                queue_size=1, slop=0.3)
+
+        self.sync.registerCallback(self.sync_callback)
+        time.sleep(0.1)
+
+
+    def sync_callback(self, leftImage, rightImage):
+        self.lock.acquire()
+        self.get_position_callback()
+        self.cameraLeft_callback(leftImage)
+        self.cameraRight_callback(rightImage)
+        self._meta_data = FrameData(left=self._cameraLeft_img, right=self._cameraRight_img, ball_pos=self._ball_pos)
+        self._observation_stale = False
+        self.lock.release()
+        self.end = time.time()
+
+
+    def cameraLeft_callback(self, data):
+        try:
+            self._cameraLeft_img = self._br.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def cameraRight_callback(self, data):
+        try:
+            self._cameraRight_img = self._br.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def get_position_callback(self):
+        rospy.wait_for_service('/gazebo/get_model_state')
+        try:
+            get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            get_position_msg = GetModelStateRequest()
+            get_position_msg.model_name = 'Ping Pong Ball'
+            state = get_state(get_position_msg)
+            self._ball_pos = (state.pose.position.x, state.pose.position.y, state.pose.position.z)
+        except rospy.ServiceException, e:
+            print("Service call failed: %s" % e)
+
+    def get_camera_data(self):
+        self.lock.acquire()
+        if self._observation_stale:
+            return_value = None
+        else:
+            self._observation_stale = True
+            return_value = self._meta_data
+        self.lock.release()
+        return return_value
+
+if __name__ == "__main__":
+    process_index = 0
+    rospy.init_node('acquire_frame_data')
+    camera = Camera()
+    SF = SaveFrame(camera=camera)
+    repeat_num = 100
+    bounds = np.array([[0.036, 0.042],
+                       [0.100, 0.1007]])
+    num = np.array([4, 2])
+    x_offset = np.arange(-0.7, 0.7, 0.05).reshape(-1, 1)
+    yz = SF.generate_force(bounds=bounds, num=num)
+    x_offset_num = x_offset.shape[0]
+    yz_num = yz.shape[0]
+    x_offset = np.repeat(x_offset, yz_num, axis=0)
+    yz = np.tile(yz, (x_offset_num, 1))
+    print 'force condition size:', yz.shape[0]
+    process_indices = np.array_split(np.arange(yz.shape[0]), 3)
+    f_start_idx = process_indices[process_index][0]
+    f_end_idx = process_indices[process_index][-1]
+    assert f_start_idx < yz.shape[0], 'f_start_idx should be less than yz.shape[0]'
+    conditions = (f_end_idx + 1 - f_start_idx) * repeat_num
+    for idx in xrange(f_start_idx, f_end_idx+1):
+        for r_idx in xrange(repeat_num):
+            start_time = time.time()
+            px = x_offset[idx] + 0.7
+            fx = 0.0
+            fy, fz = yz[idx]
+            print('Collecting data for f%d-h%d, x=%.3f, fx=%.3f, fy=%.3f, fz=%.3f ...' %
+                  (idx, r_idx, px, fx, fy, fz))
+            SF.set_ball_position(px=px, py=-0.5)
+            SF.hit_ball(fx=fx, fy=fy, fz=fz, dt=0.15)
+            meta_data = SF.acquire_frames_data(num_images=30)
+            ball_poses = SF.save_data(fx=fx, fy=fy, fz=fz,
+                                      meta_data=meta_data,
+                                      f_idx=idx,
+                                      h_idx=r_idx)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            loop_num = (idx - f_start_idx) * repeat_num + r_idx + 1
+            remaining_time = elapsed_time * (conditions - loop_num)
+            est_finished_time = datetime.now() + timedelta(seconds=remaining_time)
+            print("    Loop %d / %d took %.2f s, "
+                  "estimated finished time: \033[94m %s \033[00m" % (loop_num, conditions,
+                                                   elapsed_time,
+                                                   est_finished_time.strftime("%Y-%m-%d %H:%M:%S")))
+
+
+
+
+
+
+
+
+
---
+++ tools/convert_data.py
@@ -0,0 +1,155 @@
+import numpy as np
+import json
+import cv2
+import os
+import time
+import argparse
+
+def get_left_right_center_pixel(args, restore=False, save=True, process_index=0):
+    restore_dir = os.path.join(args.train_dir, 'processed_data')
+    center_pixel_dir = os.path.join(restore_dir, 'c_pixel')
+    if not os.path.exists(center_pixel_dir):
+        os.makedirs(center_pixel_dir)
+    if restore:
+        filename = os.path.join(center_pixel_dir, 'c_pixel_%d.json'%process_index)
+        with open(filename, 'r') as f:
+            tmp_dict = json.load(f)
+            xys = np.asarray(tmp_dict['xys'])
+            xyzs = np.asarray(tmp_dict['xyzs'])
+    else:
+        fetch_start_time = time.time()
+        data_dir = os.path.join(args.train_dir, 'raw_data')
+        force_folders = [os.path.join(data_dir, folder) for folder in os.listdir(data_dir)
+                         if os.path.isdir(os.path.join(data_dir, folder))]
+        num_frames = 58
+        xys = np.empty((0, num_frames, 4)) # center pixel in left and right image
+        xyzs = np.empty((0, num_frames, 3)) # cartesian coordinate of table tennis ball in the world
+
+        example_total_num = 0
+        for force_folder in force_folders:
+            h_folder_list = os.listdir(force_folder)
+            h_folder_list = [folder for folder in h_folder_list
+                             if os.path.isdir(os.path.join(force_folder, folder))]
+            example_total_num += len(h_folder_list)
+        example_processed = 0
+
+        for force_folder in force_folders:
+            trial_folders = [os.path.join(force_folder, folder)
+                             for folder in os.listdir(force_folder)
+                             if os.path.isdir(os.path.join(force_folder, folder))]
+            for trial_folder in trial_folders:
+                start = time.time()
+                tmp_center_pixels, tmp_coord = get_center_pixel(trial_folder, num_frames)
+                tmp_xy = np.asarray(tmp_center_pixels)
+                tmp_xyz = np.asarray(tmp_coord)
+                xys = np.concatenate((xys, np.expand_dims(tmp_xy, axis=0)), axis=0)
+                xyzs = np.concatenate((xyzs, np.expand_dims(tmp_xyz, axis=0)), axis=0)
+
+                example_processed += 1
+                end = time.time()
+                time_elapsed = end - start
+                total_time_elapsed = end - fetch_start_time
+                finished_percentage = example_processed / float(example_total_num)
+                print('Examples finished: %.2f %%, example processed time: %.2f s,'
+                      ' total time elapsed %.2f s' %
+                      (finished_percentage * 100.0, time_elapsed, total_time_elapsed))
+        if save:
+            filename = os.path.join(center_pixel_dir, 'c_pixel_%d.json'%process_index)
+            with open(filename, 'w') as f:
+                tmp_dict = {}
+                tmp_dict['xys'] = xys.tolist()
+                tmp_dict['xyzs'] = xyzs.tolist()
+                json.dump(tmp_dict, f, indent=4)
+        fetch_end_time = time.time()
+        fetch_time_elapsed = fetch_end_time - fetch_start_time
+        print("\nSummary: fetching center pixels take %.2f s" % fetch_time_elapsed)
+    print("Loading center pixels done...")
+    return xys, xyzs
+
+
+def get_center_pixel(trial_folder, num_images):
+    left_image_folder = os.path.join(trial_folder, 'left')
+    right_image_folder = os.path.join(trial_folder, 'right')
+    xyz_folder = os.path.join(trial_folder, 'xyz')
+    center_pixels = []
+    xyzs = []
+    left_fgbg = cv2.BackgroundSubtractorMOG()
+    right_fgbg = cv2.BackgroundSubtractorMOG()
+    left_images_list = os.listdir(left_image_folder)
+    left_images_list = [image for image in left_images_list if image.endswith('.jpg')]
+    assert len(left_images_list) > num_images, 'Cannot fetch %d images in %s' % (num_images, left_images_list)
+    idx = 0
+    left_bkg_image = '%s-%d.jpg' % ('left', len(left_images_list) - 1)
+    right_bkg_image = '%s-%d.jpg' % ('right', len(left_images_list) - 1)
+    left_image = cv2.imread(os.path.join(left_image_folder, left_bkg_image))
+    right_image = cv2.imread(os.path.join(right_image_folder, right_bkg_image))
+    left_fgmask = left_fgbg.apply(left_image)
+    right_fgmask = right_fgbg.apply(right_image)
+    while True:
+        left_image_name = '%s-%d.jpg' % ('left', idx)
+        right_image_name = '%s-%d.jpg' % ('right', idx)
+        xyz_filename = '%s-%d.json' % ('xyz', idx)
+        left_image = cv2.imread(os.path.join(left_image_folder, left_image_name))
+        right_image = cv2.imread(os.path.join(right_image_folder, right_image_name))
+        xyz_file = os.path.join(xyz_folder, xyz_filename)
+        left_fgmask = left_fgbg.apply(left_image)
+        right_fgmask = right_fgbg.apply(right_image)
+        left_contours, left_hierarchy = cv2.findContours(left_fgmask,
+                                                         cv2.RETR_EXTERNAL,
+                                                         cv2.CHAIN_APPROX_SIMPLE)
+        right_contours, right_hierarchy = cv2.findContours(right_fgmask,
+                                                           cv2.RETR_EXTERNAL,
+                                                           cv2.CHAIN_APPROX_SIMPLE)
+
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        left_xy_radius = []
+        right_xy_radius = []
+        for contour in left_contours:
+            if contour.shape[0] < 3:
+                continue
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            left_xy_radius.append((int(x), int(y), radius))
+        for contour in right_contours:
+            if contour.shape[0] < 3:
+                continue
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            right_xy_radius.append((int(x), int(y), radius))
+        if not left_xy_radius:
+            left_x = 0
+            left_y = 0
+            left_radius = 0
+        else:
+            left_xy_radius = sorted(left_xy_radius, key=lambda tup: tup[0])
+            left_x, left_y, left_radius = left_xy_radius[-1]
+        if not right_xy_radius:
+            right_x = 0
+            right_y = 0
+            right_radius = 0
+        else:
+            right_xy_radius = sorted(right_xy_radius, key=lambda tup: tup[0])
+            right_x, right_y, right_radius = right_xy_radius[-1]
+
+        center_pixels.append([left_x, left_y, right_x, right_y])
+        tmp_xyz = []
+        with open(xyz_file, 'r') as f:
+            data = json.load(f)
+            tmp_xyz.append(data['xyz'][0]['x'])
+            tmp_xyz.append(data['xyz'][0]['y'])
+            tmp_xyz.append(data['xyz'][0]['z'])
+        xyzs.append(tmp_xyz)
+        idx += 1
+        if idx == num_images:
+            break
+        assert len(left_images_list) > idx, 'Cannot fetch %d images in %s' % (num_images, left_image_folder)
+
+    return center_pixels, xyzs
+
+if __name__ == "__main__":
+    process_index = 0
+    parser = argparse.ArgumentParser(description='Run the Table Tennis Ball Prediction algorithm.')
+    parser.add_argument('--train_dir', type=str, default='./',
+                        help='path to the training directory.')
+    args = parser.parse_args()
+    get_left_right_center_pixel(args, restore=False, save=True, process_index=process_index)
